0e27cfab74423af5c5c7e763d5f42bb90d360f5b
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-01-08 17:09:15 +0000

    Get firebase plugged in

---
+++ src/config/config.sample.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "KEY",
+  authDomain: "DOMAIN.firebaseapp.com",
+  databaseURL: "https://DATABASE.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "BUCKET.appspot.com",
+  messagingSenderId: "SENDER_ID"
+};
+
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+import { connect } from 'react-firebase';
+
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Config from './config/config.local.js';
+
+firebase.initializeApp(Config);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
