7ad7d84bc40fae99bfcebfdfef16e99517c7f503
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-01-09 10:57:25 +0000

    Plug in a navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navigation />
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navigation />
+          <main>
+            {/* <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/> */}
+          </main>
+        </React.Fragment>
+      </Router>
     );
   }
 }
+
+
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Navigation extends Component {
+  state = {
+    navigation: {
+      '/': 'Home',
+      '/login': 'Log in',
+      '/sign-up': 'Sign up'
+    }
+  }
+
   render() {
     return (
       <nav className="nav">
         <h1>Randomals</h1>
-        <ul className="list--unstyled">
-
+        <ul className="list--unstyled list--inline">
+          {
+            Object.keys(this.state.navigation).map((key) => <li><Link to={key}>{ this.state.navigation[key] }</Link></li>)
+          }
         </ul>
       </nav>
     );
