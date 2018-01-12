b60cb161e42c25de2eb2fb3bd561666c1d61835d
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-01-12 12:29:11 +0000

    Code tidying

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import RandomalListView from './partials/RandomalListView';
+
+import '../style/list.css';
 class Home extends Component {
   render() {
     return (
@@ -9,9 +12,23 @@ class Home extends Component {
           <h1>Randomals</h1>
           <p>A test app based on th concept of creating and sharing animals</p>
         </header>
+        { this.displayLinks() }
         <RandomalListView {...this.props} />
       </React.Fragment>
     );
+  }
+
+  displayLinks = () => {
+    if (this.props.auth.user) {
+      return (
+        <nav>
+          <ul className="list--unstyled list--inline">
+            <li className="list__item"><Link to="/add" >+ Add new Randomal</Link></li>
+            <li className="list__item"><Link to="/my-list">My Randomals</Link></li>
+          </ul>
+        </nav>
+      )
+    }
   }
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import AuthStore from '../store/AuthStore';
 import Actions from '../action/auth/Actions';
